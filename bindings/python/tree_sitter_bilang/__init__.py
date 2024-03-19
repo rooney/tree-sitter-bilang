776d5aebc5eab39928ed957267e01a5836f298f1
Ron Panduwana <panduwana@gmail.com>
2024-03-19 19:18:26 +0700

    Initial commit

---
+++ bindings/python/tree_sitter_bilang/__init__.py
@@ -0,0 +1,5 @@
+"Bilang grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
---
+++ bindings/swift/TreeSitterBilang/bilang.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_BILANG_H_
+#define TREE_SITTER_BILANG_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_bilang(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_BILANG_H_
