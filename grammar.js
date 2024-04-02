b1814cbe6ca1c1f9d3d7024d165a699c8b4fdcfa
Ron Panduwana <panduwana@gmail.com>
2024-04-02 14:09:31 +0700

    feat: block graving

--- grammar.js
+++ grammar.js
@@ -5,89 +5,102 @@ module.exports = grammar({
   name: "bilang",
   rules: {
     program: ($) => seq(optional(seq($._linefeed, $._break)), $._expr3),
-    _expr3: ($) => choice($.apply3, $._expr2),
-    _expr2: ($) => choice($.apply2, $._expr1),
+    _expr3: ($) => choice($.apply3, $._expr2, $.grave3),
+    _expr2: ($) => choice($.apply2, $._expr1, $.grave2),
     _expr1: ($) => choice($.apply1, seq($._e, $._linefeed)),
     _expri: ($) => choice($.applyi, $._e),
-    apply3: ($) => seq($._expr2, $._break, $._expr3),
+
+    apply3: ($) =>
+      choice(seq($._e, $._space, $.grave3), seq($._expr2, $._break, $._expr3)),
     apply2: ($) =>
       choice(
         seq($._e, $._linefeed, $._indent, $._break, $._expr3, $._dedent),
-        seq($._e, $._space, $.apply2),
+        seq($._e, $._space, choice($.apply2, $.grave2)),
       ),
     apply1: ($) => seq($._e, $._space, $._expr1),
     applyi: ($) => seq($._e, $._space, $._expri),
+
     _e: ($) => choice($.id, $.string),
-    string: ($) =>
-      choice(
-        seq('"', repeat(choice($.stx_dquo, $.interpol8, $._linesplit)), '"'),
-        seq('"', $.linefeed, $.blockquoted, $._break, '"'),
-        seq("`", choice($._stx_words, $._stx_group)),
-        seq("` ", repeat1(choice($.stx_line, $.interpol8, $._linesplit))),
-      ),
-    _stx_words: ($) =>
-      seq($.stx_word, repeat1(choice($.stx_word, $.interpol8, $._linesplit))),
-    _stx_group: ($) => {
-      let blocs = repeat(
-        choice($._stx_group, $.stx_bloc, $.interpol8, $._linesplit),
-      );
+    string: ($) => {
+      let interpolable = (x) => choice(x, $._interpol8);
       return choice(
-        seq($.lparen, blocs, $.rparen),
-        seq($.lbrace, blocs, $.rbrace),
-        seq($.lbracket, blocs, $.rbracket),
+        seq('"', repeat(interpolable($.stx_dquo)), '"'),
+        seq("`", repeat1(interpolable($.stx_word))),
+        seq("`", $.stx_group),
+        seq("` ", $._strline),
+        seq('"', $.linefeed, $.strblock, $._break, '"'),
       );
     },
-    blockquoted: ($) =>
+    _strline: ($) => repeat1(choice($.stx_line, $._interpol8)),
+    strblock: ($) =>
       seq(
         $._indent,
-        repeat1(
-          choice(
-            $.blockquoted,
-            seq(
-              $._break,
-              repeat1(choice($.stx_line, $.interpol8, $._linesplit)),
-              $.linefeed,
-            ),
-          ),
-        ),
+        repeat1(choice($.strblock, seq($._break, $._strline, $.linefeed))),
         $._dedent,
       ),
-    interpol8: ($) => seq("`{", choice($._codeblock, $._expri), "}"),
-    interpolB: ($) => seq(" `", $._codeblock),
-
+    _graving: ($) => choice($._graveline, $.graveblock),
+    grave2: ($) => seq("`", $._linefeed, $.graveblock),
+    grave3: ($) => seq("`", $._linefeed, $._graveline, repeat($._graving)),
+    graveblock: ($) => seq($._indent, repeat1($._graving), $._dedent),
+    _graveline: ($) =>
+      seq(
+        $._break,
+        choice(
+          seq(optional($._space), $.interpolB),
+          seq(
+            repeat1(choice($.stx_rune, $._interpol8)),
+            choice($.linefeed, seq($._space, $.interpolB)),
+          ),
+        ),
+      ),
+    interpolA: ($) => seq("`{", choice($._codeblock, $._expri), "}"),
+    interpolB: ($) => seq("`", $._codeblock),
+    _interpol8: ($) => choice($.interpolA, $._linesplit),
     _codeblock: ($) =>
       seq($._linefeed, $._indent, $._break, $.program, $._dedent),
-
-    // str: raw; stx: expandable (interpolation)
+    _stx_group: ($) => {
+      let group = repeat(choice($._stx_group, $._stx_grp, $._interpol8));
+      return choice(
+        seq("(", group, ")"),
+        seq("{", group, "}"),
+        seq("[", group, "]"),
+      );
+    },
+    stx_group: ($) => $._stx_group,
     str_line: () => choice(/[^\r\n]+/),
     stx_line: () => choice(/[^\r\n`]+/, "`"),
     str_squo: () => choice(/[^\r\n']+/),
     stx_dquo: () => choice(/[^\r\n`"]+/, "`"),
-    stx_bloc: () => choice(/[^\r\n`(){}\[\]]+/, "`"),
-    stx_word: () => choice(/[^\r\n`(){}\[\] ,\t]+/, "`"),
-    id: () => /[a-z]+/,
-    _space: () => /[ \t]+/,
-    _linefeed: () => /([ \t]*\r?\n)+\t*/,
-    linefeed: ($) => $._linefeed,
-    _linesplit: ($) => seq("`{", $._linefeed, $._break, "}"),
+    stx_word: () => choice(/[^\r\n`(){}\[\], ]+/, "`"),
+    _stx_grp: () => choice(/[^\r\n`(){}\[\]]+/, "`"),
+    stx_rune: ($) =>
+      choice(
+        /[^\r\n ]+/,
+        seq("`", /[^{\r\n]/),
+        seq($._space, /[^`\r\n]/),
+        seq(optional($._space), "`", $._linefeed, $._break),
+      ),
 
-    lparen: () => "(",
-    rparen: () => ")",
-    lbrace: () => "{",
-    rbrace: () => "}",
-    lbracket: () => "[",
-    rbracket: () => "]",
+    id: () => /[a-z]+/,
+    linefeed: ($) => $._linefeed,
+    _space: () => / +/,
+    _linefeed: () => /([ \t]*\r?\n)+\t*/,
+    _linesplit: ($) => seq("`{", $._linefeed, $._break, "}"),
   },
   extras: () => [],
-  inline: () => [
-    // $._expri,
-    // $._expr1,
-    // $._expr2,
-    // $._expr3,
-    // $._e,
-    // $._codeblock,
-    // $._stx_words,
-    // $._stx_group,
+  inline: ($) => [
+    $._e,
+    $._expri,
+    $._expr1,
+    $._expr2,
+    $._expr3,
+    $._codeblock,
+    $._interpol8,
+    $._graving,
+    $._graveline,
+    $._linesplit,
+    $._strline,
+    $._stx_grp,
   ],
   externals: ($) => [$._indent, $._dedent, $._break, $._sentinel],
 });
