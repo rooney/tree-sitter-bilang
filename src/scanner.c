1744be8400be0dbc7e174ac24de58aaf4a675037
Ron Panduwana <panduwana@gmail.com>
2024-04-11 08:40:46 +0700

    perf: simplify scanner

--- src/scanner.c
+++ src/scanner.c
@@ -1,10 +1,8 @@
 #include "tree_sitter/alloc.h"
 #include "tree_sitter/parser.h"
-#include <stdint.h>
 #include <string.h>
 
 enum TokenType { INDENT, DEDENT, BREAK, SENTINEL };
-const char *token_names[3] = {"INDENT", "DEDENT", "BREAK"};
 
 void *tree_sitter_bilang_external_scanner_create() {
   return ts_calloc(1, sizeof(uint32_t));
@@ -26,46 +24,23 @@ void tree_sitter_bilang_external_scanner_deserialize(void *payload,
   memcpy(payload, buffer, length);
 }
 
-#define LOG(...) // lexer->log(lexer, __VA_ARGS__)
-static inline bool result_symbol(TSLexer *lexer, enum TokenType tt) {
-  LOG(token_names[tt]);
-  lexer->result_symbol = tt;
-  return true;
-}
-
-static inline bool decline(TSLexer *lexer, char *reason) {
-  LOG(reason);
-  return false;
-}
-
 bool tree_sitter_bilang_external_scanner_scan(void *payload, TSLexer *lexer,
                                               const bool *valid_symbols) {
   uint32_t *level = (uint32_t *)payload;
   uint32_t col = lexer->get_column(lexer);
-  uint32_t lookahead = lexer->lookahead;
-
-  LOG("scan %d level:%d col:%d expecting indent:%d dedent:%d break:%d "
-      "sentinel:%d",
-      lexer->lookahead, *level, col, valid_symbols[INDENT],
-      valid_symbols[DEDENT], valid_symbols[BREAK], valid_symbols[SENTINEL]);
 
   if (valid_symbols[SENTINEL]) {
-    return decline(lexer, "(recovery mode)");
-  }
-  if (*level > col && valid_symbols[DEDENT]) {
-    (*level)--;
-    return result_symbol(lexer, DEDENT);
-  }
-  if (lexer->eof(lexer)) {
-    return decline(lexer, "(eof)");
-  }
-  if (*level < col && valid_symbols[INDENT]) {
+    return false;
+  } else if (col > *level) {
     (*level)++;
-    return result_symbol(lexer, INDENT);
+    lexer->result_symbol = INDENT;
+  } else if (col < *level) {
+    (*level)--;
+    lexer->result_symbol = DEDENT;
+  } else if (lexer->eof(lexer)) {
+    return false;
+  } else {
+    lexer->result_symbol = BREAK;
   }
-  if (*level == col && valid_symbols[BREAK]) {
-    return result_symbol(lexer, BREAK);
-  }
-  return decline(lexer, "(other)");
+  return true;
 }
-#undef LOG
